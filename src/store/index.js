b7a4ef90106997da5c14532902f9d4a2de9d9719
lvidal1 <lvidal910@gmail.com>
2021-09-18 12:28:52 -0500

    Enable feature to persist state into localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,27 @@ import { createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from "./reducers";
+import { loadState, saveState } from "./localStorage";
 
 const loggerMiddleware = createLogger();
 
+const persistedState = loadState();
+
 export const store = createStore(
   rootReducer,
+  persistedState,
   composeWithDevTools(
-  applyMiddleware(loggerMiddleware)
+    applyMiddleware(loggerMiddleware)
   )
 );
+
+/**
+ * Defines the data to be stored on the localstorage
+ */
+store.subscribe(() => {
+  saveState({
+    comments: store.getState().comments,
+    posts: { commentIdsById: store.getState().posts.commentIdsById }
+  });
+});
+
