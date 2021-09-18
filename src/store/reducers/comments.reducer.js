f977079b5f00315b957fdb9700080f3d6a0cb692
lvidal1 <lvidal910@gmail.com>
2021-09-17 23:40:16 -0500

    Comment reducer refactored

---
+++ src/store/reducers/comments.reducer.js
@@ -0,0 +1,90 @@
+import { combineReducers } from 'redux';
+
+import { actions } from "../actions/comments.actions";
+
+/**
+ * Map metadata
+ * 
+ * @param {object} state 
+ * @param {string} action 
+ * @returns object state
+ */
+function metaReducer(
+  state = {
+    loading: false,
+    error: null,
+  }, action
+) {
+  switch (action.type) {
+    case actions.GETALL_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case actions.GETALL_SUCCESS:
+      return {
+        ...state,
+        loading: false
+      };
+    case actions.GETALL_FAILURE:
+      return {
+        ...state,
+        error: action.error
+      };
+  }
+  return state;
+}
+
+/**
+ * Map all array of comments
+ * 
+ * @param {object} state 
+ * @param {string} action 
+ * @returns object state
+ */
+export const allReducer = (state = false, action) => {
+  switch (action.type) {
+    case actions.GETALL_SUCCESS:
+      const { comments } = action;
+      return comments;
+  }
+  return state;
+}
+
+/**
+ * Normalize all commentId
+ * 
+ * @param {object} state 
+ * @param {string} action 
+ * @returns object state
+ */
+export const commentByIdReducer = (state = {}, action) => {
+  switch (action.type) {
+    case actions.GETALL_SUCCESS: {
+      const { comments } = action;
+
+      let byId = comments.reduce((map, comment) => ({ ...map, [comment.id]: comment }), {});
+      return {
+        ...state,
+        ...byId
+      };
+    }
+    case actions.ADD_SUCCESS:{
+        const { comment } = action;
+  
+        let newComment = { [comment.id] : comment  }
+        return {
+          ...state,
+          ...newComment,
+        };
+      }
+  }
+  return state;
+}
+
+
+export const commentReducer = combineReducers({
+  meta: metaReducer,
+  all: allReducer,
+  byId: commentByIdReducer,
+});
