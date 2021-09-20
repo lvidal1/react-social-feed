ba883de0b499a3edf4742f897731e70732b4015b
lvidal1 <lvidal910@gmail.com>
2021-09-20 02:13:51 -0500

    Updated Feed component

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -8,21 +8,20 @@ import Post from "../Post";
 const Feed = () => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts);
-  const {page, count} = useSelector((state) => state.posts.meta);
+  const { page, count } = useSelector((state) => state.posts.meta);
 
+  const onLoadMorePosts = () => {
+    postActions.getAll(dispatch, page, count);
+  }
+    
   useEffect(() => {
     postActions.getAll(dispatch, page, count);
   }, []);
 
-  const onLoadMorePosts = () =>{
-    postActions.getAll(dispatch, page, count);
-  }
-
   return (
     <>
       <h2 className="text-2xl font-semibold">Feed</h2>
-      {posts.meta.loading && <em>Loading posts...</em>}
-      {posts.meta.error && <span className="text-danger">ERROR: {posts.meta.error}</span>}
+      
       {posts.all &&
         <div className="space-y-2 p-4 items-center flex flex-col">
           {posts.all.map((post) =>
@@ -30,6 +29,8 @@ const Feed = () => {
           )}
         </div>
       }
+      {posts.meta.loading && <em>Loading posts...</em>}
+      {posts.meta.error && <span className="text-danger">ERROR: {posts.meta.error}</span>}
       {<a href="#!" onClick={onLoadMorePosts} >Load more posts</a>}
     </>
   );
